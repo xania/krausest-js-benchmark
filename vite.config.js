09b31be52083612f91522ac0289088cb249edb4a
Ibrahim ben Salah <Ibrahim.BenSalah@planonsoftware.com>
2022-12-08 18:03:25 +0100

    implemention of the js framework benchmark

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+﻿import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  logLevel: "info",
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  root: "src",
+  build: {
+    minify: true,
+    outDir: "../dist",
+  },
+  plugins: [tsconfigPaths()],
+});
